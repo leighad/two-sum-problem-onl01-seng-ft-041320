152f4804b28a0ce885975da1d6634561e7cb5a53
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-06-13 18:14:06 -0700

    commit tree 0c6448d917727a7736f2acc3eaf2e25a14ab0cb8

---
+++ index.js
@@ -0,0 +1,15 @@
+function bruteForceTwoSum(sum, a) {
+    a.map( x => x)
+}
+
+function binarySearchTwoSum() {
+
+}
+
+function binaryMatch() {
+    return true 
+}
+
+function hashTwoSum() {
+    
+}
